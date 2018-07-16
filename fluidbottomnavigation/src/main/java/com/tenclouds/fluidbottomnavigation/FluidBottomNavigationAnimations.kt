25991237d05f8f6d4ad43fb73642b01e21178d87
Jakub Jodełka <jakub@jodelka.me>
2018-07-16 13:20:09 +0200

    add: basic deselect animations

--- fluidbottomnavigation/src/main/java/com/tenclouds/fluidbottomnavigation/FluidBottomNavigationAnimations.kt
+++ fluidbottomnavigation/src/main/java/com/tenclouds/fluidbottomnavigation/FluidBottomNavigationAnimations.kt
@@ -4,18 +4,16 @@ import android.animation.Animator
 import android.animation.AnimatorSet
 import android.animation.ArgbEvaluator
 import android.animation.ValueAnimator
+import android.content.Context
 import android.support.v4.view.animation.LinearOutSlowInInterpolator
 import android.support.v4.view.animation.PathInterpolatorCompat
 import android.view.View
 import android.view.animation.Interpolator
 import android.view.animation.LinearInterpolator
-import android.view.animation.OvershootInterpolator
 import android.widget.ImageView
-import android.widget.TextView
-import com.tenclouds.fluidbottomnavigation.interpolator.ReverseInterpolator
 import kotlinx.android.synthetic.main.item.view.*
 
-private val selectCircleScaleInterpolations =
+private val circleScaleInterpolations =
         arrayOf(
                 arrayOf(0.250f, 0.000f, 0.000f, 1.000f),
                 arrayOf(0.420f, 0.000f, 0.580f, 1.000f),
@@ -23,12 +21,21 @@ private val selectCircleScaleInterpolations =
                 arrayOf(0.200f, 0.000f, 0.800f, 1.000f),
                 arrayOf(0.200f, 0.000f, 0.800f, 1.000f))
 
-
-private val selectIconScaleInterpolations =
+private val iconScaleInterpolations =
         arrayOf(
                 arrayOf(0.250f, 0.000f, 0.000f, 1.000f),
                 arrayOf(0.250f, 0.000f, 0.000f, 1.000f),
                 arrayOf(0.270f, 0.000f, 0.000f, 1.000f))
+
+private val itemsMoveInterpolations =
+        arrayOf(
+                arrayOf(0.250f, 0.000f, 0.000f, 1.000f),
+                arrayOf(0.500f, 0.000f, 0.500f, 1.000f))
+
+private val topContainerScaleInterpolation = arrayOf(0.250f, 0.000f, 0.000f, 1.000f)
+
+private fun getItemTranslationY(context: Context?) =
+        context?.resources?.getDimension(R.dimen.fluidBottomNavigationItemTranslationY) ?: 0f
 
 internal fun FluidBottomNavigation.animateShow() =
         AnimatorSet()
@@ -63,72 +70,116 @@ internal fun FluidBottomNavigation.animateSelectItemView(itemView: View) {
                                         circle.visible()
                                         circle.setTintColor(getColor(accentColor))
                                     },
-                            circle.getSelectCircleMoveAnimatorSet(),
-                            icon.getSelectCircleMoveAnimatorSet(), // TODO: icon has another move
+                            circle.getSelectItemsMoveAnimator(),
                             icon.getSelectIconScaleAnimatorSet(),
+                            icon.getSelectItemsMoveAnimator(),
                             icon.getSelectIconTintAnimatorSet(iconColor, iconSelectedColor),
                             title.getSelectTextAlphaAnimatorSet(),
-                            title.getSelectCircleMoveAnimatorSet(), // TODO: title has another move
-                            topAnimatedImageView.getSelectTopContainerMoveAnimatorSet())
+                            title.getSelectItemsMoveAnimator(),
+                            topAnimatedImageView.getSelectTopContainerMoveAnimatorSet(),
+                            topAnimatedImageView.getSelectTopContainerScaleAnimatorSet())
                 }
                 .start()
     }
 }
 
-// TODO we need create this animations one more time, not with reverse interpolation
 internal fun FluidBottomNavigation.animateDeselectItemView(itemView: View) {
     with(itemView) {
         AnimatorSet()
                 .apply {
                     playTogether(
                             circle
-                                    .getSelectCircleScaleAnimatorSet()
+                                    .getDeselectCircleScaleAnimatorSet()
                                     .withEndAction {
                                         circle.invisible()
                                         circle.setTintColor(getColor(backColor))
-                                    }.withReverse(),
-                            circle.getSelectCircleMoveAnimatorSet().withReverse(),
-                            icon.getSelectCircleMoveAnimatorSet().withReverse(), // TODO: icon has another move
-                            icon.getSelectIconScaleAnimatorSet().withReverse(),
-                            icon.getSelectIconTintAnimatorSet(iconColor, iconSelectedColor).withReverse(),
-                            title.getSelectTextAlphaAnimatorSet().withReverse(),
-                            title.getSelectCircleMoveAnimatorSet().withReverse(), // TODO: title has another move
-                            topAnimatedImageView.getSelectTopContainerMoveAnimatorSet().withReverse())
+                                    },
+                            circle.getDeselectItemsMoveAnimator(),
+                            icon.getDeselectIconScaleAnimatorSet(),
+                            icon.getDeselectItemsMoveAnimator(),
+                            icon.getDeselectIconTintAnimatorSet(iconSelectedColor, iconColor),
+                            title.getDeselectTextAlphaAnimatorSet(),
+                            title.getDeselectItemsMoveAnimator(),
+                            topAnimatedImageView.getDeselectTopContainerMoveAnimatorSet(),
+                            topAnimatedImageView.getDeselectTopContainerScaleAnimatorSet())
                 }
                 .start()
     }
 }
 
-private fun ImageView?.getSelectCircleScaleAnimatorSet() =
+private fun View?.getSelectCircleScaleAnimatorSet() =
         AnimatorSet()
                 .apply {
                     playSequentially(
-                            scaleAnimator(0.0f, 1.0f, 7.keyFramesToMs(), selectCircleScaleInterpolations[0].toInterpolation()),
-                            scaleAnimator(1.0f, 0.33f, 4.keyFramesToMs(), selectCircleScaleInterpolations[1].toInterpolation()),
-                            scaleAnimator(0.33f, 1.2f, 7.keyFramesToMs(), selectCircleScaleInterpolations[2].toInterpolation()),
-                            scaleAnimator(1.2f, 0.8f, 3.keyFramesToMs(), selectCircleScaleInterpolations[3].toInterpolation()),
-                            scaleAnimator(0.8f, 1.0f, 3.keyFramesToMs(), selectCircleScaleInterpolations[4].toInterpolation()))
+                            scaleAnimator(0.0f, 1.0f, 7.keyFramesToMs(), circleScaleInterpolations[0].toInterpolation()),
+                            scaleAnimator(1.0f, 0.33f, 4.keyFramesToMs(), circleScaleInterpolations[1].toInterpolation()),
+                            scaleAnimator(0.33f, 1.2f, 7.keyFramesToMs(), circleScaleInterpolations[2].toInterpolation()),
+                            scaleAnimator(1.2f, 0.8f, 3.keyFramesToMs(), circleScaleInterpolations[3].toInterpolation()),
+                            scaleAnimator(0.8f, 1.0f, 3.keyFramesToMs(), circleScaleInterpolations[4].toInterpolation()))
                 }
 
-private fun View?.getSelectCircleMoveAnimatorSet() =
+private fun View?.getDeselectCircleScaleAnimatorSet() =
+        AnimatorSet()
+                .apply {
+                    playSequentially(
+                            scaleAnimator(1.0f, 0.8f, 3.keyFramesToMs(), circleScaleInterpolations[4].toInterpolation()),
+                            scaleAnimator(0.8f, 1.2f, 3.keyFramesToMs(), circleScaleInterpolations[3].toInterpolation()),
+                            scaleAnimator(1.2f, 0.33f, 7.keyFramesToMs(), circleScaleInterpolations[2].toInterpolation()),
+                            scaleAnimator(0.33f, 1.0f, 4.keyFramesToMs(), circleScaleInterpolations[1].toInterpolation()),
+                            scaleAnimator(1.0f, 0.0f, 7.keyFramesToMs(), circleScaleInterpolations[0].toInterpolation()))
+
+                }
+
+private fun View?.getSelectIconScaleAnimatorSet() =
+        AnimatorSet()
+                .apply {
+                    playSequentially(
+                            scaleAnimator(0.9f, 1.1f, 7.keyFramesToMs(), iconScaleInterpolations[0].toInterpolation()),
+                            scaleAnimator(1.1f, 0.84f, 4.keyFramesToMs(), iconScaleInterpolations[1].toInterpolation()),
+                            scaleAnimator(0.84f, 0.9f, 4.keyFramesToMs(), iconScaleInterpolations[2].toInterpolation()))
+                }
+
+private fun View?.getDeselectIconScaleAnimatorSet() =
+        AnimatorSet()
+                .apply {
+                    playSequentially(
+                            scaleAnimator(0.9f, 0.84f, 4.keyFramesToMs(), iconScaleInterpolations[2].toInterpolation()),
+                            scaleAnimator(0.84f, 1.1f, 4.keyFramesToMs(), iconScaleInterpolations[1].toInterpolation()),
+                            scaleAnimator(1.1f, 0.9f, 7.keyFramesToMs(), iconScaleInterpolations[0].toInterpolation()))
+                }
+
+
+private fun View?.getSelectItemsMoveAnimator() =
         AnimatorSet()
                 .apply {
                     playSequentially(
                             translationYAnimator(
                                     0f,
-                                    -(this@getSelectCircleMoveAnimatorSet?.resources?.getDimension(R.dimen.fluidBottomNavigationItemTranslationY)
-                                            ?: 0f), 11.keyFramesToMs(), OvershootInterpolator()))
+                                    -(getItemTranslationY(this@getSelectItemsMoveAnimator?.context) * 11 / 10),
+                                    7.keyFramesToMs(),
+                                    itemsMoveInterpolations[0].toInterpolation()),
+                            translationYAnimator(
+                                    -(getItemTranslationY(this@getSelectItemsMoveAnimator?.context) * 11 / 10),
+                                    -(getItemTranslationY(this@getSelectItemsMoveAnimator?.context)),
+                                    3.keyFramesToMs(),
+                                    itemsMoveInterpolations[1].toInterpolation()))
                     startDelay = 11.keyFramesToMs()
                 }
 
-
-private fun ImageView?.getSelectIconScaleAnimatorSet() =
+private fun View?.getDeselectItemsMoveAnimator() =
         AnimatorSet()
                 .apply {
                     playSequentially(
-                            scaleAnimator(0.9f, 1.1f, 7.keyFramesToMs(), selectIconScaleInterpolations[0].toInterpolation()),
-                            scaleAnimator(1.1f, 0.84f, 4.keyFramesToMs(), selectIconScaleInterpolations[1].toInterpolation()),
-                            scaleAnimator(0.84f, 0.9f, 4.keyFramesToMs(), selectIconScaleInterpolations[2].toInterpolation()))
+                            translationYAnimator(
+                                    -(getItemTranslationY(this@getDeselectItemsMoveAnimator?.context)),
+                                    -(getItemTranslationY(this@getDeselectItemsMoveAnimator?.context) * 11 / 10),
+                                    3.keyFramesToMs(),
+                                    itemsMoveInterpolations[1].toInterpolation()),
+                            translationYAnimator(
+                                    -(getItemTranslationY(this@getDeselectItemsMoveAnimator?.context) * 11 / 10),
+                                    0f,
+                                    7.keyFramesToMs(),
+                                    itemsMoveInterpolations[0].toInterpolation()))
                 }
 
 private fun ImageView?.getSelectIconTintAnimatorSet(colorFrom: Int?,
@@ -141,18 +192,61 @@ private fun ImageView?.getSelectIconTintAnimatorSet(colorFrom: Int?,
                             3.keyFramesToMs()))
                 }
 
-private fun TextView?.getSelectTextAlphaAnimatorSet() =
+private fun ImageView?.getDeselectIconTintAnimatorSet(colorFrom: Int?,
+                                                      colorTo: Int?) =
+        AnimatorSet()
+                .apply {
+                    play(tintAnimator(
+                            getColor(colorFrom),
+                            getColor(colorTo),
+                            3.keyFramesToMs()))
+                    startDelay = 20.keyFramesToMs()
+                }
+
+private fun View?.getSelectTextAlphaAnimatorSet() =
         AnimatorSet()
                 .apply {
                     play(alphaAnimator(0f, 1f, 8.keyFramesToMs(), LinearOutSlowInInterpolator()))
-                            .apply { startDelay = 11.keyFramesToMs() }
+                    startDelay = 14.keyFramesToMs()
+                }
+
+private fun View?.getDeselectTextAlphaAnimatorSet() =
+        AnimatorSet()
+                .apply {
+                    play(alphaAnimator(1f, 0f, 8.keyFramesToMs(), LinearOutSlowInInterpolator()))
+                    startDelay = 1.keyFramesToMs()
                 }
 
 private fun ImageView?.getSelectTopContainerMoveAnimatorSet() =
         AnimatorSet()
                 .apply {
-                    play(translationYAnimator(100f, 8f, 7.keyFramesToMs(), selectIconScaleInterpolations[1].toInterpolation()))
-                            .apply { startDelay = 11.keyFramesToMs() }
+                    play(translationYAnimator(100f, 8f, 7.keyFramesToMs(), iconScaleInterpolations[1].toInterpolation()))
+                    startDelay = 11.keyFramesToMs()
+                }
+
+private fun ImageView?.getDeselectTopContainerMoveAnimatorSet() =
+        AnimatorSet()
+                .apply {
+                    play(translationYAnimator(8f, 100f, 7.keyFramesToMs(), iconScaleInterpolations[1].toInterpolation()))
+                    startDelay = 4.keyFramesToMs()
+                }
+
+private fun ImageView?.getSelectTopContainerScaleAnimatorSet() =
+        AnimatorSet()
+                .apply {
+                    playSequentially(
+                            scaleAnimator(0.9f, 1.05f, 1.keyFramesToMs(), itemsMoveInterpolations[0].toInterpolation()),
+                            scaleAnimator(1.05f, 0.9f, 2.keyFramesToMs(), itemsMoveInterpolations[1].toInterpolation()))
+                    startDelay = 16.keyFramesToMs()
+                }
+
+private fun ImageView?.getDeselectTopContainerScaleAnimatorSet() =
+        AnimatorSet()
+                .apply {
+                    playSequentially(
+                            scaleAnimator(1.05f, 0.9f, 2.keyFramesToMs(), topContainerScaleInterpolation.toInterpolation()),
+                            scaleAnimator(0.9f, 1.05f, 1.keyFramesToMs(), topContainerScaleInterpolation.toInterpolation()))
+                    startDelay = 1.keyFramesToMs()
                 }
 
 private fun View?.scaleAnimator(from: Float = this?.scaleX ?: 0f,
@@ -230,10 +324,6 @@ private fun AnimatorSet.withEndAction(function: () -> Unit) =
                     })
                 }
 
-private fun AnimatorSet.withReverse() = this.apply { interpolator = ReverseInterpolator() }
-
 private fun Int.keyFramesToMs() = ((this.toFloat() / 24f) * 1000).toLong()
 
 private fun Array<Float>.toInterpolation() = PathInterpolatorCompat.create(this[0], this[1], this[2], this[3])
-
-
